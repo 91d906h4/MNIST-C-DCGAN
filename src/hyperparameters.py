4735134bdccf7e5086482f94c21cc33e0c0f7593
91d906h4 <fairymanning@gmail.com>
2024-04-17 01:57:13 +0900

    init: create hyperparameters.py

---
+++ src/hyperparameters.py
@@ -0,0 +1,9 @@
+# Set Hyper-Parameters.
+
+# Dataset.
+dataset_path    = "../dataset"  # The path of dataset.
+batch_size      = 16            # Batch size.
+
+# Model.
+epochs          = 1             # Epochs.
+learning_rate   = 0.00001       # Learning rate.
