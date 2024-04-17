03ce07a2754e9e13c3b691b3556380aab9fcd002
91d906h4 <fairymanning@gmail.com>
2024-04-17 16:47:44 +0900

    update: update hyperparameters.py

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -4,6 +4,10 @@
 dataset_path    = "../dataset"  # The path of dataset.
 batch_size      = 128           # Batch size.
 
+# Optimizer.
+beta1           = 0.5           # Beta values for Adam.
+beta2           = 0.999
+learning_rate   = 1e-5          # Learning rate.
+
 # Model.
 epochs          = 10            # Epochs.
-learning_rate   = 0.00001       # Learning rate.
